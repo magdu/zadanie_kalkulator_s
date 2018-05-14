8ddcdd6b0648de15894e1cd7afc1e8979e3e5643
Magdalena <magdabiala@gmail.com>
2018-05-14 12:19:51 +0200

    Some working version

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import{Component}from'@angular/core';
+import {HttpClient}from '@angular/common/http';
+import {HttpParams}from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Demo';
+  dailyRate;
+  country = 'PL';
+  countries =[
+      { name: 'Poland', symbol: 'PL' },
+      { name: 'Germany', symbol: 'DE' },
+      { name: 'Great Britain', symbol: 'UK' }
+      ];
+  data = {};
+
+  constructor(private http: HttpClient) { }
+
+  onCompute(){
+    let params = new HttpParams().set('dailyRate', this.dailyRate).set('country',this.country);
+    this.http.get('rate', { params: params }).subscribe(data => this.data = data);
+  }
+}
